a219427925828d1a1b281a6c66f02794f6a4c76b
kaarthika1409 <154883233+kaarthika1409@users.noreply.github.com>
2025-01-31 09:05:00 +0530

    Update signupSchema.js

--- backend/models/signupSchema.js
+++ backend/models/signupSchema.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const signupSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true, // Ensure email is required
-        unique: true,   // Ensure email is unique
-        trim: true,     // Remove extra spaces
-        lowercase: true // Convert email to lowercase
+        required: true, 
+        unique: true,  
+        trim: true,     
+        lowercase: true 
     },
     password: {
         type: String,
-        required: true // Ensure password is required
+        required: true
     },
    
 });
